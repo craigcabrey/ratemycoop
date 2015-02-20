5ab4692c3ab08ee859bfa7890101744a9f695040
kocsen <kocsenc@gmail.com>
2015-02-19 20:50:47 -0500

    yeoman initial setup

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ratemycoopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ratemycoopApp
+ */
+angular.module('ratemycoopApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
