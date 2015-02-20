233d9cb995551abc3da389ec90cdfdbc98c176b3
kocsen <kocsenc@gmail.com>
2015-02-20 09:06:43 -0500

    - company prototype page - add padding bottom main.css -  add company js and company html

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -27,6 +27,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/company', {
+        templateUrl: 'views/company.html',
+        controller: 'CompanyCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ client/app/scripts/controllers/company.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ratemycoopApp.controller:CompanyCtrl
+ * @description
+ * # CompanyCtrl
+ * Controller of the ratemycoopApp
+ */
+angular.module('ratemycoopApp')
+  .controller('CompanyCtrl', function ($scope) {
+    $("#rating").rating();
+    $("#moneyRating").rating()
+
+  });
