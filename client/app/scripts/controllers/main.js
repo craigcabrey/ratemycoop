9dc79db6707856a50f5de023d392fc315a86907b
kocsen <kocsenc@gmail.com>
2015-02-19 22:59:05 -0500

    - ready for slider

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -9,9 +9,6 @@
  */
 angular.module('ratemycoopApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+
   });
