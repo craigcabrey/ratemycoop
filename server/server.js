76b0857042a19ce83b8ebca6659f5a6da173d8b7
Craig Cabrey <craigcabrey@gmail.com>
2015-02-22 19:46:33 -0500

    Add logging to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 var loopback = require('loopback');
+var morgan = require('morgan');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
+
+app.middleware('initial', morgan('short'));
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
