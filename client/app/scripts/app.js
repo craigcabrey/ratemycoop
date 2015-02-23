51b9de19e8a83199d2c5b41e7f8891edbe693c6f
Craig Cabrey <craigcabrey@gmail.com>
2015-02-23 13:30:09 -0500

    Add angular-filter dependency to bower

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -16,6 +16,7 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
+    'angular.filter',
     'lbServices'
   ])
   .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
