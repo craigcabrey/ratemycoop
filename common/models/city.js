e89dcfc5f219131257410ddc180c84756652a570
Craig Cabrey <craigcabrey@gmail.com>
2015-02-25 18:54:16 -0500

    Add search remote method to City model

--- common/models/city.js
+++ common/models/city.js
@@ -1,3 +1,25 @@
 module.exports = function(City) {
+  City.search = function(query, callback) {
+    // TODO: sanitize query
+    City.find(
+      {
+        where: {
+          name: {
+            like: '%' + query + '%'
+          }
+        }
+      }, function (err, results) {
+        callback(null, results);
+      }
+    );
+  };
 
+  City.remoteMethod(
+    'search',
+    {
+      http: { verb: 'get' },
+      accepts: { arg: 'query', type: 'string' },
+      returns: { arg: 'results', type: [ 'object' ] }
+    }
+  );
 };
