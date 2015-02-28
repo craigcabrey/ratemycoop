75c37e64a4da1732e0cac155ffefa0fa6b61980e
Craig Cabrey <craigcabrey@gmail.com>
2015-02-28 09:59:35 -0500

    Model updates

--- common/models/company.js
+++ common/models/company.js
@@ -1,17 +1,21 @@
 module.exports = function(Company) {
-  Company.beforeCreate = function(next, modelInstance) {
-    Company.findOne({
-        where: { name: modelInstance.name }
-      },
-      function(err, instance) {
-        if (err || instance) {
-          next(new Error('Company already exists.'));
-        } else {
-          next();
+  Company.observe('before save', function(ctx, next) {
+    if (ctx.instance) {
+      Company.findOne({
+          where: { name: ctx.instance.name }
+        },
+        function(err, company) {
+          if (err || company) {
+            next(new Error('Company already exists.'));
+          } else {
+            next();
+          }
         }
-      }
-    );
-  };
+      );
+    } else {
+      next();
+    }
+  });
 
   Company.afterRemote(
     '**',
