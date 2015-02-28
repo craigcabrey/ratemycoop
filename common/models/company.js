8b1a3f7d7eb898eebe4a59ef0695bc12b82ffea0
Craig Cabrey <craigcabrey@gmail.com>
2015-02-27 22:04:02 -0500

    Prevent creation of duplicate companies.

--- common/models/company.js
+++ common/models/company.js
@@ -1,15 +1,32 @@
 module.exports = function(Company) {
+  Company.beforeCreate = function(next, modelInstance) {
+    Company.findOne({
+        where: { name: modelInstance.name }
+      },
+      function(err, instance) {
+        if (err || instance) {
+          next(new Error('Company already exists.'));
+        } else {
+          next();
+        }
+      }
+    );
+  };
+
   Company.afterRemote(
     '**',
     function(ctx, instance, next) {
       if (instance.reviews) {
-        instance.reviews().forEach(
-          function(review) {
-            if (review.anonymous) {
-              review.userId = null;
+        var reviews = instance.reviews();
+        if (reviews) {
+          reviews().forEach(
+            function(review) {
+              if (review.anonymous) {
+                review.userId = null;
+              }
             }
-          }
-        );
+          );
+        }
       }
 
       next();
