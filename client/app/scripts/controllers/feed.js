8c7e40bc086a54f5eff91fe59ac39b84ded1d951
Kocsen <kocsenc@gmail.com>
2015-03-07 18:27:16 -0500

    scaffold feed

--- client/app/scripts/controllers/feed.js
+++ client/app/scripts/controllers/feed.js
@@ -9,9 +9,25 @@
  */
 angular.module('ratemycoopApp')
   .controller('FeedCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    $scope.companies = Company.find({},
+      function (success) {// Search trigger
+        angular.forEach(success, function (value) {
+          var newVal = value;
+          newVal['title'] = value.name;
+          newVal['url'] = "/#/company/" + value.name;
+          if (value.description && value.description.length >= 230) {
+            newVal['description'] = value.description.substr(0, 230) + "...";
+          }
+          if (value.description === null) { // For when the desciption is null
+            newVal['description'] = "";
+          }
+        });
+
+
+      }
+    );
+
+
+
   });
