a5372793f3c3ce04ed681084dfa5dda301bac33e
Kocsen <kocsenc@gmail.com>
2015-03-07 16:16:00 -0500

    - retreieved app.js for api

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -60,8 +60,7 @@ angular
     //$locationProvider.html5Mode({ enabled: true, requireBase: false });
 
     // Set url base
-    //LoopBackResourceProvider.setUrlBase('https://ratemycoop.io/api/v1');
-    LoopBackResourceProvider.setUrlBase('http://localhost:3000/api/v1');
+    LoopBackResourceProvider.setUrlBase('https://ratemycoop.io/api/v1');
 
 
     /**
