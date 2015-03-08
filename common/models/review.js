213091ccc634d1c3613b17a11f95839e4349e752
Joseph DeSimpliciis <jcdesimp@aol.com>
2015-03-07 23:52:58 -0500

    added endpoint "isLikedByUser" to see if current user has liked the review.

--- common/models/review.js
+++ common/models/review.js
@@ -1,68 +1,127 @@
-module.exports = function(Review) {
+module.exports = function (Review) {
 
   var loopback = require('loopback');
 
   Review.afterRemote(
     'access',
-    function(ctx, instance, next) {
+    function (ctx, instance, next) {
       console.log(instance.likes());
-      ctx.Model.include = function(subItems, subInclude, callback) { callback(); };
+      ctx.Model.include = function (subItems, subInclude, callback) {
+        callback();
+      };
       next();
     }
   );
 
-  //Review.afterRemote(
-  //  'like',
-  //
-  //);
 
-  //Review.prototype.like = function(user) {
-  //  var self = this;
-  //  self.likes({
-  //      where: {
-  //        userId: user.id
-  //      }
-  //    },
-  //    function(err, likes) {
-  //      console.log(likes);
-  //    }
-  //  );
-  //};
+  /**
+   * Remote method to have a user like/unlike a review.
+   * @param cb
+   */
+  Review.prototype.like = function (cb) {
 
-  Review.prototype.like = function(cb) {
+    var self = this;                // reference to this review
+    var app = Review.app;           // reference to the loopback app
+    var ctx = loopback.getCurrentContext();
+    var currUser = ctx && ctx.get('currentUser'); // get the current user
 
-    var self = this;
+    if (currUser) {                 // see if there is a current user (is user logged in?)
+
+      //check if user review of company exists already
+      app.models.ReviewLike.findOne(
+        {
+          where: {
+            userId: currUser.id,
+            reviewId: self.id
+          }
+        },
+        function (err, theLike) {
+          if (!err) {
+            if (theLike) {
+              app.models.ReviewLike.destroyById(theLike.id, function (err) {
+                if (!err) {
+                  // review has been unliked.
+                  cb(null, "Unliked", false);
+                } else {
+                  cb(err);
+                }
+              });
+
+            } else {
+              self.likes.create(
+                {
+                  user: currUser
+                },
+                function (err) {
+                  if (!err) {
+                    // Review has been liked
+                    cb(null, "Liked", true);
+                  } else {
+                    cb(err);
+                  }
+                }
+              );
+            }
+          } else {
+            cb(err);
+          }
+        });
+
+    } else {
+      var error = new Error("User not authenticated");
+      error.status = 403;
+      cb(error);
+    }
+  };
+
+  /**
+   * Remote method to see if a user has liked this review
+   * @param cb
+   */
+  Review.prototype.isLikedByUser = function (cb) {
+    var self = this;          // reference to this review
+    var app = Review.app;     // reference to the Loopback app
     var ctx = loopback.getCurrentContext();
     var currUser = ctx && ctx.get('currentUser');
+    if (currUser) {
+      app.models.ReviewLike.findOne(
+        {
+          where: {
+            userId: currUser.id,
+            reviewId: self.id
+          }
+        },
+        function (err, theLike) {
+          if (!err) {
+            if (theLike) {
+              // is liked
+              cb(null, true);
+            } else {
+              // is not liked
+              cb(null, false);
 
-    var app = Review.app;
+            }
+          } else {
+            cb(err);
+          }
+        });
+    } else {
+      // construct "unauthenticated" error
+      var error = new Error("User not authenticated");
+      error.status = 403;
+      cb(error);
+    }
 
-
-    self.likes.create(
-      {
-        user: currUser
-      }
-    );
-
-    console.log(currUser);
-
-
-    cb(null, "Success");
 
   };
 
-  Review.prototype.unlike = function(user) {
-    var self = this;
-
-  };
-
-  Review.observe('before save', function(ctx, next) {
+  Review.observe('before save', function (ctx, next) {
     var loopback = require('loopback');
     var newctx = loopback.getCurrentContext();
     // Force userId to be that of the logged in user
-    if(newctx !== null){
+    if (newctx !== null) {
       var currentUser = newctx && newctx.get('currentUser');
-      if(currentUser !== undefined){
+      if (currentUser !== undefined) {
         ctx.instance.userId = currentUser.id;
 
         // Check that user has not submitted a review the the past 3 months
@@ -77,8 +136,8 @@ module.exports = function(Review) {
               }
             }
           },
-          function(err, review) {
-            if(review) {
+          function (err, review) {
+            if (review) {
               var error = new Error('You\'ve already reviewed this company in the past three months.');
               error.status = 403;
               next(error);
@@ -100,9 +159,25 @@ module.exports = function(Review) {
   Review.remoteMethod(
     'like',
     {
-      returns: {arg: "msg", type: "string"},
-      isStatic: false
+      returns: [{arg: "msg", type: "string"}, {arg: "isLiked", type: "boolean"}],
+      isStatic: false,
+      description: "Like or unlike this review as the current user.s"
     }
   );
+
+  Review.remoteMethod(
+    'isLikedByUser',
+    {
+      returns: [{arg: "isLiked", type: "boolean"}],
+      isStatic: false,
+      http: {
+        verb: "get"
+      },
+      description: "Check if this review was liked by the current user."
+    }
+  );
+
+  // Disable unused methods
+  Review.disableRemoteMethod('deleteLikeById', true);
 
 };
