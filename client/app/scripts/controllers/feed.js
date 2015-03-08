6c7c2273016ab55f9a302295dbb17e303e00da3f
Kocsen <kocsenc@gmail.com>
2015-03-07 20:30:48 -0500

    better template

--- client/app/scripts/controllers/feed.js
+++ client/app/scripts/controllers/feed.js
@@ -10,16 +10,25 @@
 angular.module('ratemycoopApp')
   .controller('FeedCtrl', function ($scope, Company) {
 
-    $scope.companies = Company.find({},
+    $scope.companies = Company.find({
+        filter: {
+          limit: 2,
+          include: ['reviews', {'locations': 'region'}]
+        }
+      },
       function (success) {// Search trigger
+
+
+        console.log($scope.companies[0]);
         angular.forEach(success, function (value) {
+
           var newVal = value;
           newVal['title'] = value.name;
           newVal['url'] = "/#/company/" + value.name;
           if (value.description && value.description.length >= 230) {
             newVal['description'] = value.description.substr(0, 230) + "...";
           }
-          if (value.description === null) { // For when the desciption is null
+          if (value.description === null) { // For when the description is null
             newVal['description'] = "";
           }
         });
@@ -27,7 +36,6 @@ angular.module('ratemycoopApp')
 
       }
     );
-
 
 
   });
