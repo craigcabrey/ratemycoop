a6ee02b03af7651490658800ba66e3331bdc6379
Kocsen <kocsenc@gmail.com>
2015-03-08 20:43:43 -0400

    - validation set up and bindings set up

--- client/app/scripts/controllers/createcompany.js
+++ client/app/scripts/controllers/createcompany.js
@@ -26,37 +26,81 @@ angular.module('ratemycoopApp')
 
 
     $scope.submitNewCompany = function () {
-      var data = $scope.suggestCompanyEditForm;
-      data.loading = true;
-      Company.create(
-        {
-          "name": data.name,
-          "url": data.url,
-          "logo": data.logo,
-          "facebook": data.facebook,
-          "twitter": data.twitter,
-          "linkedin": data.linkedin,
-          "seekingFulltime": data.seekingFulltime,
-          "seekingCoop": data.seekingCoop,
-          "description": data.description,
-          minPay: null,
-          maxPay: null
-        },
-        function (success) {
-          data.loading = false;
-          $location.path('/company/' + data.name);
-        },
-        function (error) {
-          data.loading = false;
-          data.error = true;
-        }
-      )
+      if ($('#newCompanyForm').form('validate form')) {
+        var data = $scope.suggestCompanyEditForm;
+        data.loading = true;
+        Company.create(
+          {
+            "name": data.name,
+            "url": data.url,
+            "logo": data.logo,
+            "facebook": data.facebook,
+            "twitter": data.twitter,
+            "linkedin": data.linkedin,
+            "seekingFulltime": data.seekingFulltime,
+            "seekingCoop": data.seekingCoop,
+            "description": data.description,
+            minPay: null,
+            maxPay: null
+          },
+          function (success) {
+            data.loading = false;
+            $location.path('/company/' + data.name);
+          },
+          function (error) {
+            data.loading = false;
+            data.error = true;
+          }
+        )
+      }
     };
 
 
     // Semantic Triggers .ready() block.
     $(document).ready(function () {
       $('.ui.checkbox').checkbox();
+
+
+      $('#newCompanyForm').form(
+        {
+          name: {
+            identifier: 'companyEdit-name',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter a company name.'
+              }
+            ]
+          },
+          description: {
+            identifier: 'companyEdit-description',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter a company description'
+              }
+            ]
+          },
+          site: {
+            identifier: 'companyEdit-url',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter a company website'
+              },
+              {
+                type: 'url',
+                prompt: 'Please enter a valid url (with http://)'
+              }
+            ]
+          }
+
+        },
+        {
+          on: 'blur'
+          //inline: true
+        }
+      )
     });
 
 
